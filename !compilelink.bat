e8b6231b94e5cb4aa3d0b591138f3a12afd10a9f
Nikita Zimin <nzeemin@gmail.com>
2019-11-06 03:43:58 +0300

    WIP: JMP -> BR, now we have 172 bytes left.

--- !compilelink.bat
+++ !compilelink.bat
@@ -1,4 +1,6 @@
 @echo off
+set rt11exe=C:\bin\rt11\rt11.exe
+set pclink11=C:\bin\pclink11.exe
 
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
@@ -13,7 +15,7 @@ echo 	.IDENT /V%REVISION%/ >> VERSIO.MAC
 @if exist EXPRES.LST del EXPRES.LST
 @if exist EXPRES.OBJ del EXPRES.OBJ
 
-C:\bin\rt11\rt11.exe MACRO/LIST:DK: EXPRES.MAC
+%rt11exe% MACRO/LIST:DK: EXPRES.MAC
 
 for /f "delims=" %%a in ('findstr /B "Errors detected" EXPRES.LST') do set "errdet=%%a"
 if "%errdet%"=="Errors detected:  0" (
@@ -24,14 +26,31 @@ if "%errdet%"=="Errors detected:  0" (
   exit /b
 )
 
-@if exist OUTPUT.MAP del OUTPUT.MAP
+@if exist EXPRES.MAP del EXPRES.MAP
 @if exist EXPRES.SAV del EXPRES.SAV
+@if exist EXPRES-11.MAP del EXPRES-11.MAP
+@if exist EXPRES-11.SAV del EXPRES-11.SAV
 
-C:\bin\rt11\rt11.exe LINK EXPRES /MAP:OUTPUT.MAP
+%rt11exe% LINK EXPRES /MAP:EXPRES.MAP
+@if exist EXPRES.MAP rename EXPRES.MAP EXPRES-11.MAP
+@if exist EXPRES.SAV rename EXPRES.SAV EXPRES-11.SAV
 
-for /f "delims=" %%a in ('findstr /B "Undefined globals" OUTPUT.MAP') do set "undefg=%%a"
+%pclink11% EXPRES.OBJ /MAP > pclink11.log
+
+fc.exe /b EXPRES-11.SAV EXPRES.SAV > fc.log
+for /f "delims=" %%a in ('findstr /B "FC: " fc.log') do set "fcdiff=%%a"
+if "%fcdiff%"=="FC: no differences encountered" (
+  echo SAV FILES ARE EQUAL
+  del fc.log
+  echo.
+) ELSE (
+  echo ======= SAV FILES ARE DIFFERENT, see fc.log =======
+  exit /b
+)
+
+for /f "delims=" %%a in ('findstr /B "Undefined globals" EXPRES.MAP') do set "undefg=%%a"
 if "%undefg%"=="" (
-  type OUTPUT.MAP
+  type EXPRES.MAP
   echo.
   echo LINKED SUCCESSFULLY
   echo.
